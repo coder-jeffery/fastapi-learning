02b2b9430c8e8e7a9a73b5d2ff6a0f5af27785c3
19901712802 <123456jcm$>
2026-01-18 00:13:54 +0800

    python dict & tuple

--- python3-leanring/python3learning.py
+++ python3-leanring/python3learning.py
@@ -28,6 +28,24 @@ class pythonlearning:
     print(f'********************************************************')
 
 
+    var_list = [1,2,3,4,5,6,7,8,9]
+    print(type(var_list), var_list)
+    print(f'********************************************************')
+    var_data = {'llamaindex','LangChain', 'LangGraph', 'Milvus', 'postgresql'}
+    print(type(var_data), var_data)
+
+    var_dictionary = {}
+    var_tuple = ('pytorch', 'tensorflow', 'python', 'django', 'fastapi', 'flask')
+    print(type(var_tuple), var_tuple, var_tuple[0])
+
+    for item in var_tuple:
+        var_dictionary[item] = var_tuple.index(item)
+        # var_dictionary[item] = var_tuple[var_tuple.index(item)]
+        print(var_dictionary[item])
+
+    print(f'********************************************************')
+
+
 print(pythonlearning())
 print(pythonlearning().learning_basetype())
 print(f'中文显示主题内容')
