7a8581f4a6e68eb7068f5e4eb3f343efc690801f
19901712802 <123456jcm$>
2026-01-18 00:05:49 +0800

    python basetype

---
+++ python3-leanring/python3learning.py
@@ -0,0 +1,36 @@
+
+class pythonlearning:
+
+    a = 1
+
+    def __init__(self):
+        print("pythonlearning...")
+
+
+    def learning_basetype(self):
+        print("learning_basetype...")
+
+    b  =10
+    llamaindex = 'this is large language model'
+    test = bool
+    pai = 3.1415926
+    money = 999999999999999.1234
+    number = [1,2,3,4,5,6,7,8,9]
+
+
+
+
+    print(type(a), a,type(llamaindex), llamaindex, type(b), b)
+    print(f'********************************************************')
+    print(type(money), type(number), type(pai))
+    print(f'********************************************************')
+    print(f'python基本数据类型： 数字 number，字符串 string，列表 list， 元祖 tuple， 字典 dictionary')
+    print(f'********************************************************')
+
+
+print(pythonlearning())
+print(pythonlearning().learning_basetype())
+print(f'中文显示主题内容')
+
+
+
